42608f007dcf3833b9bb8c255183dbfe92b943fb
首藤典宏 <norihiro-MBP@norihiro-MBP.local>
2018-05-22 22:12:46 +0900

    first commit app

---
+++ ExCsvParser/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExCsvParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start Read CSV");
+
+            //var enumCsv =
+            //    from field in
+            //        UtilityCsv.Context(@"C:\WORK\PG\C#\ExCsvParser\ExCsvParser\bin\Debug\CSV\47OKINAW.CSV",
+            //        ",", Encoding.GetEncoding(932))
+            //    select new string[] { field[2], field[6], field[7], field[8] };
+
+            IEnumerable<string[]> results =
+                from csvpath in
+                    Directory.GetFiles("CSV_FILES", "*.csv", System.IO.SearchOption.TopDirectoryOnly)
+                from fields in
+                    UtilityCsv.Context(csvpath, ",", Encoding.GetEncoding(932))
+                select new string[] { fields[2], fields[6], fields[7], fields[8] };
+
+            var xx = Directory.GetFiles("CSV_FILES", "*.csv", System.IO.SearchOption.TopDirectoryOnly)
+                .SelectMany(csvPath => UtilityCsv.Context(csvPath, ",", Encoding.GetEncoding(932))
+                )
+                .Select(fields => new string[] { fields[2], fields[6] });
+
+            var x = UtilityCsv.Context(@"CSV_FILES\47OKINAW.CSV", ",", Encoding.GetEncoding(932))
+                .Select(fields => new string[]{fields[2], fields[6]});
+
+            UtilityCsv.WriteFileCsv(
+                @"CONVERT_CSV\Converted.CSV",
+                xx);
+
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ ExCsvParser/UtilityCsv.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+using System.IO;
+using System.Data;
+using System.Collections;
+
+namespace ExCsvParser
+{
+    public static class UtilityCsv
+    {
+        public static IEnumerable<string[]> Context(
+            string path, string separator = ",", Encoding encoding = null)
+        {
+            using (Stream stream =
+                new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using (TextFieldParser parser =
+                new TextFieldParser(stream, encoding ?? Encoding.UTF8, true, false))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.Delimiters = new[] { separator };
+                parser.HasFieldsEnclosedInQuotes = true;
+                parser.TrimWhiteSpace = true;
+                while (parser.EndOfData == false)
+                {
+                    string[] fields = parser.ReadFields();
+                    yield return fields;
+                }
+            }
+        }
+
+
+        public static DataTable ConvertToDataTable(IEnumerable<string[]> context)
+        {
+            DataTable dataTable = new DataTable();
+
+            foreach (string[] line in context)
+            {
+                int clumnNo = 0;
+                DataRow row = dataTable.NewRow();
+                foreach (string column in line)
+                {
+                    row[clumnNo] = column;
+                    clumnNo++;
+                }
+                dataTable.Rows.Add(row);
+            }
+
+            return dataTable;
+        }
+
+        public static DataSet MakeDataSet(IEnumerable<DataTable> enumDataTable)
+        {
+            DataSet dataSet = new DataSet();
+            foreach (var dt in enumDataTable)
+            {
+                dataSet.Tables.Add(dt);
+            }
+
+            return dataSet;
+        }
+
+        public static void WriteFileCsv(string path, IEnumerable<string[]> context)
+        {
+            using (StreamWriter sw = new StreamWriter(path, false, Encoding.GetEncoding(932)))
+            {
+                foreach (var line in context)
+                {
+                    string csvLine = string.Join(",", line);
+                    sw.WriteLine(csvLine);
+                }
+            }
+        }
+    }
+}
